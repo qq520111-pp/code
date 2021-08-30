e5782e449bc51ae6b54023f9c42eca80e360948e
哈哈哈 <2443209156@qq.com>
2021-08-30 18:34:49 +0800

    node

---
+++ node/readline/index.js
@@ -0,0 +1,26 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+/**
+ * interface obj {
+ *      name: string
+ *      age: number
+ *      sex: string
+ *      [properties]: any
+ * }
+ * 
+ */
+let userMsg = {}
+
+readline.question(`你叫什么名字?`, name => {
+    userMsg.name = name
+    readline.question(`你的年龄多大?`, age => {
+        userMsg.age = age
+        readline.question(`你的性别?`, sex => {
+            userMsg.sex = sex
+            console.log(userMsg);
+            readline.close()
+        })
+    })
+})
